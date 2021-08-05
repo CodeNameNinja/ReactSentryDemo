402416d584add2bb5fbbc43db44dbda25bb475c4
Mitchell <mwhyuen@gmail.com>
2021-08-05 09:28:08 +0200

    fix: remove redundant imports

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import * as Sentry from "@sentry/react";
 
 function App() {
   return <button onClick={methodDoesNotExist}>Break the world</button>;
 }
 
-export default App;
+
+export default Sentry.withProfiler(App);
