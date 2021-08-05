88e2e76f35e1938e3dd8b808eb7a14b3fee45e9c
Mitchell <mwhyuen@gmail.com>
2021-08-05 09:02:23 +0200

    feat: add sentry with first error

--- src/index.js
+++ src/index.js
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
+
+Sentry.init({
+  dsn: "https://7b63c18f358f430cb259ad773c03ef5a@o945225.ingest.sentry.io/5893833",
+  integrations: [new Integrations.BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(
   <React.StrictMode>
